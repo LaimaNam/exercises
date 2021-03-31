a524c92945aca50ff816a56d966db4f28abd25b4
Laima <laiman@Laimas-MacBook-Pro.local>
2021-03-31 21:47:08 +0300

    adding written program based on Luhn algorithm (1part)

--- think-like-a-programmer/app.js
+++ think-like-a-programmer/app.js
@@ -75,3 +75,72 @@ const triangle = () => {
 };
 
 console.log(triangle());
+
+/* 
+Luhn algorithm: write a program that takes an identification number of arbitrary length
+and determines wether the number is valid under the Luhn formula.
+First: 
+- COMPUTING check DIGIT of a given number to understant the  L algorithm 
+*/
+
+// 1--------- func which doubles and sums num
+const doubleDigit = (someNum) => {
+  let doubleDigit = someNum * 2;
+  let sum;
+  if (doubleDigit >= 10) {
+    sum = 1 + (doubleDigit % 10);
+  } else {
+    sum = doubleDigit;
+  }
+  return sum;
+};
+
+// 2 ----------
+// func where the values of each digit are summed and the check digit is found
+const sum = (arrayOfNums) => {
+  let sumOfNum = 0;
+  for (i = 0; i < arrayOfNums.length; i++) {
+    sumOfNum += arrayOfNums[i];
+  }
+
+  return 10 - (sumOfNum % 10);
+};
+
+const luhnAlgo = (num) => {
+  let arrOfNum = Array.from(String(num), Number);
+  let arrLength = arrOfNum.length;
+  let newarr = [];
+  let sumOfNum = 0;
+
+  for (i = 0; i < arrLength; i++) {
+    // 1 -  which digits to double? (every second from right to left,
+    // first digit is always 1)
+    let digit = arrOfNum[arrLength - i - 1];
+    if (i % 2 === 0) {
+      let dig = doubleDigit(digit);
+      newarr.push(dig);
+    } else {
+      newarr.push(digit);
+    }
+  }
+
+  console.log(newarr + " " + "naujas supushint'as");
+  let reversedArrr = newarr.reverse();
+  console.log(reversedArrr + " " + "koks turi but ");
+  sumOfNum = sum(reversedArrr);
+  reversedArrr.push(sumOfNum);
+  console.log(num + " " + "pradinis");
+  console.log(sumOfNum);
+
+  return reversedArrr;
+};
+
+console.log(luhnAlgo(1111));
+
+/* 
+Second: 
+Checking if the given number is correct according to Luhn algorithm 
+If not: 
+- REMOVING wrong LAST DIGIT
+- COMPUTING LAST DIGIT with the func i wrote above
+*/
