e8bc0010cf7663258f1f81b44ed9640a3d04bfa8
Laima <laiman@Laimas-MacBook-Pro.local>
2021-04-01 16:03:44 +0300

    Luhn algorithm exercise corrections

--- think-like-a-programmer/app.js
+++ think-like-a-programmer/app.js
@@ -97,12 +97,11 @@ const doubleDigit = (someNum) => {
 
 // 2 ----------
 // func where the values of each digit are summed and the check digit is found
-const sum = (arrayOfNums) => {
+const findCheckDigit = (arrayOfNums) => {
   let sumOfNum = 0;
   for (i = 0; i < arrayOfNums.length; i++) {
     sumOfNum += arrayOfNums[i];
   }
-
   return 10 - (sumOfNum % 10);
 };
 
@@ -122,9 +121,8 @@ const luhnAlgo = (num) => {
     }
   }
 
-  // let reversedArrr = newarr.reverse();
-  let sumOfNum = sum(newarr);
-  arrOfNum.push(sumOfNum);
+  let checkDigit = findCheckDigit(newarr);
+  arrOfNum.push(checkDigit);
   arrOfNum = arrOfNum.join("");
   return arrOfNum;
 };
@@ -138,3 +136,42 @@ If not:
 - REMOVING wrong LAST DIGIT
 - COMPUTING LAST DIGIT with the func i wrote above
 */
+const sum = (numb) => {
+  let sumOfNum = 0;
+  for (i = 0; i < numb.length; i++) {
+    sumOfNum += numb[i];
+  }
+  return sumOfNum;
+};
+
+const luhnChecksumFormula = (numbers) => {
+  let array = Array.from(String(numbers), Number);
+  let checkDigit = array.pop();
+
+  let arrLength = array.length;
+  let newarr = [];
+
+  for (i = 0; i < arrLength; i++) {
+    let digit = array[arrLength - i - 1];
+    if (i % 2 === 0) {
+      let dig = doubleDigit(digit);
+      newarr.push(dig);
+    } else {
+      newarr.push(digit);
+    }
+  }
+
+  let sumOfNum = sum(newarr);
+
+  if ((sumOfNum + checkDigit) % 10 === 0) {
+    array.push(checkDigit);
+    array = array.join("");
+    return `Your check digit is correct: ${array}`;
+  } else {
+    let digit = findCheckDigit(newarr);
+    array.push(digit);
+    array = array.join("");
+    return `Your check digit is incorrect so I made new one: ${array}`;
+  }
+};
+console.log(luhnChecksumFormula(11114));
